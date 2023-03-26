223d67e657cb555681cfd01256344c4a9da7745d
Andrej Orešnik <aoresnik7@gmail.com>
2023-03-26 23:58:53 +0200

    Server prototype

---
+++ babysitter-server-prototype/src/test/java/xyz/aoresnik/babysitter/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package xyz.aoresnik.babysitter;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
