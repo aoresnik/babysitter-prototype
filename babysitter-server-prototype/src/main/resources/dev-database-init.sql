fcbc02170d741c7cf3de5942cf90c98a5aa06bf7
Andrej Orešnik <aoresnik7@gmail.com>
2023-05-12 02:04:14 +0200

    Local dir as a script source

--- babysitter-server-prototype/src/main/resources/dev-database-init.sql
+++ babysitter-server-prototype/src/main/resources/dev-database-init.sql
@@ -4,3 +4,21 @@ create table TEST(
     primary key (ID)
 );
 
+create table SCRIPT_SOURCE(
+     ID int not null auto_increment,
+     NAME varchar(255) not null,
+     primary key (ID)
+);
+
+create table SCRIPT_SOURCE_SERVER_DIR(
+     ID int not null auto_increment,
+     SCRIPT_SOURCE_ID int not null,
+     DIR_NAME varchar(255) not null,
+     primary key (ID),
+     foreign key (SCRIPT_SOURCE_ID) references SCRIPT_SOURCE(ID)
+);
+
+insert into SCRIPT_SOURCE (NAME) values ('Local dir on server');
+
+insert into SCRIPT_SOURCE_SERVER_DIR (SCRIPT_SOURCE_ID, DIR_NAME) values (select ID from SCRIPT_SOURCE where NAME='Local dir on server', '/home/user/scripts');
+
