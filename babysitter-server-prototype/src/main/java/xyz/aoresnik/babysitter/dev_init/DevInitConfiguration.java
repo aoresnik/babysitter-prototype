af1eb259448d08fc04ebe16a26e5b0f91d5fc090
Andrej Orešnik <aoresnik7@gmail.com>
2023-05-14 22:19:17 +0200

    Store ssh config into the database during startup

--- babysitter-server-prototype/src/main/java/xyz/aoresnik/babysitter/dev_init/DevInitConfiguration.java
+++ babysitter-server-prototype/src/main/java/xyz/aoresnik/babysitter/dev_init/DevInitConfiguration.java
@@ -10,10 +10,9 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -53,9 +52,30 @@ public class DevInitConfiguration {
             throw new RuntimeException("Unable to open database", e);
         }
 
+        // Read SSH config files for connecting to vagrant test VMs that have been prepared by scripts called from the Vagrantfile
         List<ScriptSourceSSHDir> selectSssdFromScriptSourceSSHDirSssd = em.createQuery("select sssd from ScriptSourceSSHDir sssd").getResultList();
         for (ScriptSourceSSHDir sssd : selectSssdFromScriptSourceSSHDirSssd) {
             log.info("Found ScriptSourceSSHDir: " + sssd);
+            String name = sssd.getScriptSource().getName();
+            String sshConfigNameBase = name.split("\\ ")[0];
+            log.debug(String.format("For name %s the SSH config resource file name base is %s", name, sshConfigNameBase));
+            String sshConfigFileName = sshConfigNameBase + ".dev.ssh-config";
+
+            try (InputStream inputStream = getClass().getResourceAsStream("/" + sshConfigFileName)) {
+                if (inputStream != null) {
+                    try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                        String sshConfigFileContents;
+                        sshConfigFileContents = reader.lines()
+                                .collect(Collectors.joining(System.lineSeparator()));
+                        log.debug("Read SSH config resource from " + sshConfigFileName);
+                        sssd.setSshConfig(sshConfigFileContents.getBytes(StandardCharsets.UTF_8));
+                    }
+                } else {
+                    log.warn("Unable to find SSH config for connection to dev test-target-vm " + sshConfigFileName + " - scripts execution via SSH will not be available");
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Unable to read config resource " + sshConfigFileName,e);
+            }
         }
     }
 
