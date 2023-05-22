647add1620edd211d6e6494ef57552e25539527e
Andrej Orešnik <aoresnik7@gmail.com>
2023-05-22 02:00:57 +0200

    Test links and parsing of path params

--- babysitter-prototype/src/app/app-command-execution-view/app-command-execution-view.component.ts
+++ babysitter-prototype/src/app/app-command-execution-view/app-command-execution-view.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-app-command-execution-view',
   templateUrl: './app-command-execution-view.component.html',
   styleUrls: ['./app-command-execution-view.component.css']
 })
-export class AppCommandExecutionViewComponent {
+export class AppCommandExecutionViewComponent implements OnInit {
+  scriptSourceID?: string | null;
+  scriptID?: string | null;
+  executionID?: string | null;
 
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    console.log("AppCommandViewComponent.ngOnInit");
+    this.scriptSourceID = this.route.snapshot.paramMap.get('source_id');
+    this.scriptID = this.route.snapshot.paramMap.get('script_id');
+    this.executionID = this.route.snapshot.paramMap.get('execution_id');
+  }
 }
