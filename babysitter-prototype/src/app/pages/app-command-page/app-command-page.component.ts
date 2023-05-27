94068a53b6cf81e0758f98d687143f9966b20c1d
Andrej Orešnik <aoresnik7@gmail.com>
2023-05-27 09:42:27 +0200

    Fixed: navigation on route change

--- babysitter-prototype/src/app/pages/app-command-page/app-command-page.component.ts
+++ babysitter-prototype/src/app/pages/app-command-page/app-command-page.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-app-command-page',
   templateUrl: './app-command-page.component.html',
   styleUrls: ['./app-command-page.component.css']
 })
-export class AppCommandPageComponent {
+export class AppCommandPageComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {
+  }
 
+  scriptSourceID?: string | null;
+  scriptID?: string | null;
+
+  ngOnInit() {
+    console.log("AppCommandViewComponent.ngOnInit");
+
+    this.route.paramMap
+      .subscribe(paramMap => {
+          console.log(paramMap);
+          this.scriptSourceID = paramMap.get('source_id');
+          this.scriptID = paramMap.get('script_id');
+        }
+      );
+  }
 }
