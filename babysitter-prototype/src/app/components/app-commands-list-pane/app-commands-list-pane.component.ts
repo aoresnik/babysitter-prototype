290bdff7861dcddbdea5d2f2a4a8e763a9fef43b
Andrej Orešnik <aoresnik7@gmail.com>
2023-05-27 08:39:05 +0200

    List available commands on All Commands page

--- babysitter-prototype/src/app/components/app-commands-list-pane/app-commands-list-pane.component.ts
+++ babysitter-prototype/src/app/components/app-commands-list-pane/app-commands-list-pane.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {UITreeNode} from "primeng/tree";
 import {TreeNode} from "primeng/api";
+import {ScriptsServiceService} from "../../scripts-service.service";
+import {ScriptRunSessionService} from "../../script-run-session.service";
 
 @Component({
   selector: 'app-app-commands-list-pane',
@@ -8,18 +10,30 @@ import {TreeNode} from "primeng/api";
   styleUrls: ['./app-commands-list-pane.component.css']
 })
 export class AppCommandsListPaneComponent implements OnInit {
-  commands: TreeNode[] = [ {
-    key: '0',
-    label: 'Local commands',
-    data: 'Documents Folder',
-    icon: 'pi pi-fw pi-inbox',
-    children: [
-      { key: '0-0-0', label: 'test.sh', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
-      { key: '0-0-1', label: 'test2.sh', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
-    ]
-  } ];
+  // commandsAsTree: TreeNode[] = [ {
+  //   key: '0',
+  //   label: 'Local commands',
+  //   data: 'Documents Folder',
+  //   icon: 'pi pi-fw pi-inbox',
+  //   children: [
+  //     { key: '0-0-0', label: 'test.sh', icon: 'pi pi-fw pi-file', data: 'Expenses Document' },
+  //     { key: '0-0-1', label: 'test2.sh', icon: 'pi pi-fw pi-file', data: 'Resume Document' }
+  //   ]
+  // } ];
+  scriptsList?: any[];
+
+  constructor(private scriptsService: ScriptsServiceService) {
+
+  }
 
   ngOnInit(): void {
-    this.commands.push();
+    this.scriptsService.getScripts().subscribe(res => {
+      console.log(res);
+      this.scriptsList = res;
+    });
+  }
+
+  selectCommand(script: any) {
+
   }
 }
