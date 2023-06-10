727e3bfd83abb1f6e00594ed88c4d571ebe5cb92
Andrej Orešnik <aoresnik7@gmail.com>
2023-06-10 17:57:25 +0200

    Cmd babysitting session websocket messages included in OpenAPI spec
    Even though OpenAPI as of now cannot describe WebSocket protocol. They are
    included because they are useful to be generated in the frontend.

--- babysitter-server-prototype/src/main/java/xyz/aoresnik/babysitter/data/CommandExecutionUpdateRTData.java
+++ babysitter-server-prototype/src/main/java/xyz/aoresnik/babysitter/data/CommandExecutionUpdateRTData.java
@@ -1,11 +1,16 @@
 package xyz.aoresnik.babysitter.data;
 
 import lombok.Data;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * Included in the OpenAPI spec, because it's useful to have the interface class available in the frontend.
+ */
 @Data
 @XmlRootElement
+@Schema(description = "Data used via websocket for updates of command execution from backend to frontend")
 public class CommandExecutionUpdateRTData extends AbstractScriptExecutionRTData {
 
     private byte[] incrementalConsoleData;
--- babysitter-server-prototype/src/main/java/xyz/aoresnik/babysitter/data/CommandInputData.java
+++ babysitter-server-prototype/src/main/java/xyz/aoresnik/babysitter/data/CommandInputData.java
@@ -3,10 +3,15 @@ package xyz.aoresnik.babysitter.data;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
+/**
+ * Included in the OpenAPI spec, because it's useful to have the interface class available in the frontend.
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "Data used via websocket for sending interaction coomands from user from frontend to backend")
 public class CommandInputData {
     private String inputData;
 }
