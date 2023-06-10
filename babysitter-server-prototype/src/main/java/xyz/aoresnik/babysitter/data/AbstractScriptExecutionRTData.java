38552bf02e70263df5ab46c9748595e942b8fa1e
Andrej Orešnik <aoresnik7@gmail.com>
2023-06-10 18:30:49 +0200

    Renamed remaining script -> command in Websocket data

--- babysitter-server-prototype/src/main/java/xyz/aoresnik/babysitter/data/AbstractScriptExecutionRTData.java
+++ babysitter-server-prototype/src/main/java/xyz/aoresnik/babysitter/data/AbstractScriptExecutionRTData.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AbstractScriptExecutionRTData {
-    private String scriptExecutionID;
+    private String commandExecutionId;
 
     private boolean commandRun;
 
